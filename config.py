b1f6098f9b17499b33c50b84f65fb3fc9e08bbe6
Vasylynahel <vasilinkailcuk@gmail.com>
2026-06-04 12:45:10 +0000

    chapter-03-04:web forms, database

---
+++ config.py
@@ -0,0 +1,10 @@
+#  app = Flask(__name__)
+#  app.config['SECRET_KEY'] = 'you-will-never-guess'
+# ...and more variables here as needed
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
