ed2d5b00b44089b6df6371a4763db8970d88207c
Vasylynahel <vasilinkailcuk@gmail.com>
2026-06-05 12:02:00 +0000

    chapter-08

--- app/__init__.py
+++ app/__init__.py
@@ -46,4 +46,4 @@ if not app.debug:  # програма без режиму налагодженн
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-  from app import routes, models, errors
+from app import routes, models, errors
